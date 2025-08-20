ab7dbb16f338db09b15c4772416206bcf7f9fd96
NosiphoNdzinisa <nosiphondzinisa@gmail.com>
2025-08-20 22:13:08 +0200

    Updated the style and added more cities on the select

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -2,27 +2,54 @@ function updateTime() {
   //Los Angeles
   let losAngelesElement = document.querySelector("#los-angeles");
   if (losAngelesElement) {
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
-}
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   //Paris
   let ParisElement = document.querySelector("#paris");
   if (ParisElement) {
-  let ParisDateElement = ParisElement.querySelector(".date");
-  let ParisTimeElement = ParisElement.querySelector(".time");
-  let ParisTime = moment().tz("Europe/Paris");
+    let ParisDateElement = ParisElement.querySelector(".date");
+    let ParisTimeElement = ParisElement.querySelector(".time");
+    let ParisTime = moment().tz("Europe/Paris");
 
-  ParisDateElement.innerHTML = ParisTime.format("MMMM Do YYYY");
-  ParisTimeElement.innerHTML = ParisTime.format("h:mm:ss [<small>]A[</small>]"
-);
-}
+    ParisDateElement.innerHTML = ParisTime.format("MMMM Do YYYY");
+    ParisTimeElement.innerHTML = ParisTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  //Japan
+  let japanElement = document.querySelector("#japan");
+  if (japanElement) {
+    let japanDateElement = japanElement.querySelector(".date");
+    let japanTimeElement = japanElement.querySelector(".time");
+    let japanTime = moment().tz("Asia/Tokyo");
+
+    japanDateElement.innerHTML = japanTime.format("MMMM Do YYYY");
+    japanTimeElement.innerHTML = japanTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  //South Africa
+  let southAfricaElement = document.querySelector("#south-africa");
+  if (southAfricaElement) {
+    let southAfricaDateElement = southAfricaElement.querySelector(".date");
+    let southAfricaTimeElement = southAfricaElement.querySelector(".time");
+    let southAfricaTime = moment().tz("Africa/Johannesburg");
+
+    southAfricaDateElement.innerHTML = southAfricaTime.format("MMMM Do YYYY");
+    southAfricaTimeElement.innerHTML = southAfricaTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
